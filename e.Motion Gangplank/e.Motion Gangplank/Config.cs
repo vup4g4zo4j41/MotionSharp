bf303228bf541b6ec64a9fabcefcbcf19056c41b
Ruben <ruben.simons@web.de>
2016-07-31 00:15:52 +0200

    Initial Release

--- e.Motion Gangplank/e.Motion Gangplank/Config.cs
+++ e.Motion Gangplank/e.Motion Gangplank/Config.cs
@@ -32,6 +32,7 @@ namespace e.Motion_Gangplank
             comboMenu.AddItem(new MenuItem("combo.e", "Use E").SetValue(true));
             comboMenu.AddItem(new MenuItem("combo.ex", "Use E to Extend").SetValue(true));
             comboMenu.AddItem(new MenuItem("combo.r", "Use R").SetValue(true));
+            comboMenu.AddItem(new MenuItem("combo.rmin", "Minimum enemies for R").SetValue(new Slider(3, 2, 5)));
             Menu.AddSubMenu(comboMenu);
 
             //Lasthit
@@ -43,12 +44,36 @@ namespace e.Motion_Gangplank
             //Killsteal
             Menu killstealMenu = new Menu("Killsteal","killsteal");
             killstealMenu.AddItem(new MenuItem("killsteal.q ", "Use Q").SetValue(true));
-            killstealMenu.AddItem(new MenuItem("killsteal.qe", "Use Q on Barrel").SetValue(true));
+            Menu.AddSubMenu(killstealMenu);
 
             //Drawings
             Menu drawingMenu = new Menu("Drawings","drawings");
             drawingMenu.AddItem(new MenuItem("drawings.warning", "Remember me to Upgrade Ult").SetValue(true));
             Menu.AddSubMenu(drawingMenu);
+
+            //Cleanse
+            Menu cleanseMenu = new Menu("Cleanse","cleanse");
+            cleanseMenu.AddItem(new MenuItem("cleanse.w", "Use W to Cleanse").SetValue(true));
+
+            Menu cleanseBuffs = new Menu("Enable Cleanse for:","cleanse.bufftypes");
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.slow", "Slow").SetValue(false));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.poison", "Poison").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.blind", "Blind").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.silence", "Silence").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.stun", "Stun").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.fear", "Fear").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.polymorph", "Polymorph").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.snare", "Snare").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.taunt", "Taunt").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.suppression", "Suppression").SetValue(true));
+            cleanseBuffs.AddItem(new MenuItem("cleanse.bufftypes.charm", "Charm").SetValue(true));
+            cleanseMenu.AddSubMenu(cleanseBuffs);
+
+            Menu specialSkills = new Menu("Special Buffs","cleanse.special");
+            specialSkills.AddItem(new MenuItem("cleanse.special.placeholder", "Will be added soon"));
+            cleanseMenu.AddSubMenu(specialSkills);
+
+            Menu.AddSubMenu(cleanseMenu);
 
             //Misc
             Menu miscMenu = new Menu("Miscellanious", "misc");
@@ -56,8 +81,10 @@ namespace e.Motion_Gangplank
             miscMenu.AddItem(new MenuItem("misc.reactionTime", "enemy Reaction Time").SetTooltip("Higher = Possible not to hit enemy with Barrel, Lower = Possible to use additional Barrels").SetValue(new Slider(100,0,500)));
             miscMenu.AddItem(new MenuItem("misc.additionalReactionTime", "Additional Reaction Time on Direction Change").SetTooltip("For Calculation when enemy will change direction").SetValue(new Slider(50, 0, 200)));
             miscMenu.AddItem(new MenuItem("misc.trye", "Always extend with E").SetValue(true).SetTooltip("Will always try to Extend with E to get additional Champions"));
+            miscMenu.AddItem(new MenuItem("misc.autoE", "Place Barrels automatically").SetTooltip("Will automatically place Barrels in Bushes if no other Barrels exist").SetValue(true));
             Menu.AddSubMenu(miscMenu);
 
+            
             Menu.AddToMainMenu();
         }
 
--- e.Motion Gangplank/e.Motion Gangplank/Program.cs
+++ e.Motion Gangplank/e.Motion Gangplank/Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
+using LeagueSharp.Common.Data;
 using SharpDX;
 using Color = System.Drawing.Color;
 
@@ -23,10 +24,38 @@ namespace e.Motion_Gangplank
         #region Declaration
 
         private static int BarrelTime;
+        private static Random Rand = new Random();
+        private static DelayManager QDelay;
+
+        private static Dictionary<string, BuffType> Buffs = new Dictionary<string, BuffType>()
+        {
+            {"charm",BuffType.Charm},
+            {"slow",BuffType.Slow },
+            {"poison",BuffType.Poison},
+            {"blind",BuffType.Blind},
+            {"silence",BuffType.Silence},
+            {"stun",BuffType.Stun},
+            {"fear",BuffType.Fear},
+            {"polymorph",BuffType.Polymorph},
+            {"snare",BuffType.Snare},
+            {"taunt",BuffType.Taunt},
+            {"suppression",BuffType.Suppression}
+        };
+        private static readonly List<Vector2> BarrelPositions = new List<Vector2>()
+        {
+            new Vector2(1205, 12097),
+            new Vector2(1335, 12468),
+            new Vector2(1577, 12820),
+            new Vector2(1872, 13011),
+            new Vector2(2252, 13299),
+            new Vector2(2632, 13520)
+        };
         public static Spell Q, W, E, R;
         public static Obj_AI_Hero Player => ObjectManager.Player;
         public static List<Barrel> AllBarrel = new List<Barrel>();
         public static Vector3 EnemyPosition;
+        
+        
         #endregion
 
 
@@ -45,6 +74,7 @@ namespace e.Motion_Gangplank
             Config Menu = new Config();
             Menu.Initialize();
             #endregion
+            QDelay = new DelayManager(Q,1500);
             Game.PrintChat("e.Motion Gangplank loaded");
             SetBarrelTime();
             //Game.PrintChat("<font color='#bb0000'>e</font>.<font color='#0000cc'>Motion</font> Gangplank loaded");
@@ -59,6 +89,7 @@ namespace e.Motion_Gangplank
             Obj_AI_Base.OnDoCast += CheckForBarrel;
             Obj_AI_Base.OnNewPath += OnNewPath;
             Obj_AI_Base.OnLevelUp += OnLevelUp;
+            
             //Obj_AI_Base.OnDelete += OnDelete;
 
             #endregion
@@ -103,7 +134,17 @@ namespace e.Motion_Gangplank
 
         private static void KillSteal()
         {
-            //if(Config.Item("killsteal.q").GetValue<bool>())
+            //if (Config.Item("killsteal.q").GetValue<bool>() && Q.IsReady())
+            {
+                foreach (var enemy in HeroManager.Enemies)
+                {
+                    if (enemy.Health < Q.GetDamage(enemy) && Player.Distance(enemy) <= Q.Range)
+                    {
+                        Q.Cast(enemy);
+                    }
+                }
+            }
+            
         }
 
         private static void CheckForBarrel(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
@@ -115,7 +156,7 @@ namespace e.Motion_Gangplank
                     
                     if (AllBarrel.ElementAt(i).GetBarrel().NetworkId == args.Target.NetworkId)
                     {
-                        Game.PrintChat("Barrel Time reduced");
+                        //Game.PrintChat("Barrel Time reduced");
                         if (sender.IsMelee)
                         {
                             AllBarrel.ElementAt(i).ReduceBarrelAttackTick();
@@ -177,18 +218,48 @@ namespace e.Motion_Gangplank
 
         private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            
+            if (sender.IsMe && args.Slot == SpellSlot.E)
+            {
+                Game.PrintChat("x: " + args.End.X  +", y: " + args.End.Y);
+            }
         }
 
         private static void GameOnUpdate(EventArgs args)
         {
+            KillSteal();
+            QDelay.CheckEachTick();
+            AutoE();
             CleanBarrel();
             Combo();
             Lasthit();
+            Cleanse();
             //Game.PrintChat(E.IsReady(1).ToString());
             //QEDebug();
             //Game.PrintChat("Delay on Q: "+Q.Delay);
-            
+
+        }
+
+        private static void AutoE()
+        {
+            //Auto E - Static List
+            if (Config.Item("misc.autoE").GetValue<bool>() && E.IsReady() && E.Instance.Ammo > 1 && !AllBarrel.Any(b => b.GetBarrel().Distance(Player) <= 1200))
+            {
+                Obj_AI_Hero target = TargetSelector.GetTarget(1400,TargetSelector.DamageType.Physical);
+                IEnumerable<Vector2> possiblePositions = BarrelPositions.Where(pos => pos.Distance(Player) <= E.Range);
+                if (target != null && possiblePositions.Count() != 0)
+                {
+                    float minDist = 2000;
+                    Vector2 castPos = Vector2.Zero;
+                    foreach (var pos in possiblePositions.Where(pos => pos.Distance(target) < minDist))
+                    {
+                        castPos = new Vector2(pos.X + Rand.Next(0,21) - 10, pos.Y + Rand.Next(0,21) - 10);
+                        minDist = pos.Distance(Player);
+                    }
+                    E.Cast(castPos);
+                    
+                }
+                
+            }
         }
 
         private static void Warning()
@@ -200,19 +271,7 @@ namespace e.Motion_Gangplank
                 Drawing.DrawText(200,200,Color.Red,"Don't forget to buy Ultimate Upgrade with Silver Serpents");
             }
         }
-        //private static void QEDebug()
-        //{
-        //    if (Q.IsReady())
-        //    {
-        //        for (int i = 0; i < AllBarrel.Count; i++)
-        //        {
-        //            if (AllBarrel.ElementAt(i).CanQNow() && Player.Position.Distance(AllBarrel.ElementAt(i).GetBarrel().Position) <= Q.Range)
-        //            {
-        //                Q.Cast(AllBarrel.ElementAt(i).GetBarrel());
-        //            }
-        //        }
-        //    }
-        //}
+        
 
         private static void TryE(Barrel barrel, Obj_AI_Hero toIgnore)
         {
@@ -223,11 +282,16 @@ namespace e.Motion_Gangplank
             if (HeroManager.Enemies.FirstOrDefault(
                 e =>
                     e != toIgnore && !e.IsZombie && !e.IsDead && e.Distance(barrelPos) < 1200 &&
-                    !barrelPos.CannotEscape(e) && !GetBarrelsInRange(barrel).Any(b => b.GetBarrel().Position.CannotEscape(e,false,true)) && Helper.GetPredPos(e) &&
+                    !barrelPos.CannotEscape(e) &&
+                    !GetBarrelsInRange(barrel).Any(b => b.GetBarrel().Position.CannotEscape(e, false, true)) &&
+                    Helper.GetPredPos(e) &&
                     (castPos =
                         barrelPos.Extend(Helper.PredPos.To3D(), Math.Min(650, Player.Distance(Helper.PredPos.To3D()))))
-                        .CannotEscape(e,false,true)) != null)
+                        .CannotEscape(e, false, true)) != null)
+            {
                 E.Cast(castPos);
+                Game.PrintChat("Got Additional Targets");
+            }
         }
 
         private static void Combo(bool extended = false,Obj_AI_Hero sender = null)
@@ -236,11 +300,20 @@ namespace e.Motion_Gangplank
             {
                 return;
             }
-            
+
+            if (Config.Item("combo.r").GetValue<bool>())
+            {
+                Obj_AI_Hero RTarget = HeroManager.Enemies.FirstOrDefault(t => t.CountAlliesInRange(660) > 0);
+                if (RTarget != null)
+                {
+                    R.CastIfWillHit(RTarget, Config.Item("combo.rmin").GetValue<Slider>().Value);
+                }
+            }
+
             if (Config.Menu.Item("combo.qe").GetValue<bool>() /* && Q.IsReady() */)
             {
                 Obj_AI_Hero target = TargetSelector.GetTarget(1200, TargetSelector.DamageType.Physical);
-                if (target != null)
+                if (target != null && Q.IsReady())
                 {
                     EnemyPosition = target.Position;
                     Helper.GetPredPos(target);
@@ -248,19 +321,19 @@ namespace e.Motion_Gangplank
                     {
                         extended = false;
                     }
-                    if (!Q.IsReady()) return;
                     foreach (var b in AllBarrel)
                     {
                         
                         if (b.CanQNow() && (b.GetBarrel().Position.CannotEscape(target, extended) || GetBarrelsInRange(b).Any(bb => bb.GetBarrel().Position.CannotEscape(target, extended,true))))
                         {
                             TryE(b, target);
-                            Q.Cast(b.GetBarrel());
+                            QDelay.Delay(b.GetBarrel());
                             Game.PrintChat("Casted on Extended basis: "+(extended?"yes":"no"));
-                            return;
+                            break;
                         }
                     }
-                    if (E.IsReady() && Config.Item("combo.ex").GetValue<bool>())
+
+                    if (E.IsReady() && !QDelay.Active() && Config.Item("combo.ex").GetValue<bool>())
                     {
                         foreach (var b in AllBarrel)
                         {
@@ -273,29 +346,47 @@ namespace e.Motion_Gangplank
                                     .Distance(Player.Position) < 1000 && castPos.CannotEscape(target, extended, true))
                             {
                                 E.Cast(castPos);
-                                Q.Cast(b.GetBarrel());
+                                QDelay.Delay(b.GetBarrel());
                                 Game.PrintChat("Casted on Extended basis: " + (extended ? "yes" : "no"));
-                                return;
+                                break;
                             }
                         }
                     }
                 }
+                
             }
 
             if (Config.Item("combo.q").GetValue<bool>() && Q.IsReady())
             {
                 Obj_AI_Hero target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
-                if (target != null && (E.Cooldown <= Q.Instance.Cooldown - BarrelTime || Config.Item("key.q").GetValue<KeyBind>().Active) && !AllBarrel.Any(b => b.GetBarrel().Distance(Player) < 1200))
+                if (target != null && (E.Cooldown >= Q.Instance.Cooldown - BarrelTime || Config.Item("key.q").GetValue<KeyBind>().Active) && !AllBarrel.Any(b => b.GetBarrel().Distance(Player) < 1200))
                 {
                     Q.Cast(target);
                 }
             }
+            if (E.IsReady() && E.Instance.Ammo > 1 && Config.Item("combo.e").GetValue<bool>() && !AllBarrel.Any(b => b.GetBarrel().Position.Distance(Player.Position) <= 1200))
+            {
+                Obj_AI_Hero target = TargetSelector.GetTarget(1000,TargetSelector.DamageType.Physical);
+                if (target == null) return;
+                //Game.PrintChat("code here");
+                Helper.GetPredPos(target);
+                Vector2 castPos = target.Position.Extend(Helper.PredPos.To3D(), 200).To2D();
+                if (Player.Distance(castPos) <= E.Range)
+                {
+                    E.Cast(castPos);
+                }
+                else
+                {
+                    E.Cast(Player.Position.Extend(castPos.To3D(), 1000));
+                }
+            }
         }
-        
+
         private static IEnumerable<Barrel> GetBarrelsInRange (Barrel initalBarrel)
         {
-            return (AllBarrel.Where(b => b.GetBarrel().Position.Distance(initalBarrel.GetBarrel().Position) < 650 && b != initalBarrel));
+            return AllBarrel.Where(b => b.GetBarrel().Position.Distance(initalBarrel.GetBarrel().Position) < 650 && b != initalBarrel);
         }
+
         private static void Lasthit()
         {
             if (Config.Orbwalker.ActiveMode != Orbwalking.OrbwalkingMode.LastHit)
@@ -306,7 +397,7 @@ namespace e.Motion_Gangplank
             {
                 foreach (Barrel barrel in AllBarrel)
                 {
-                    if (barrel.CanQNow() && MinionManager.GetMinions(barrel.GetBarrel().Position,650).Any(m => m.Health < Q.GetDamage(m)))
+                    if (barrel.CanQNow() && MinionManager.GetMinions(barrel.GetBarrel().Position,650).Any(m => m.Health < Q.GetDamage(m) && m.Distance(barrel.GetBarrel()) <= 380))
                     {
                         Q.Cast(barrel.GetBarrel());
                     }
@@ -317,6 +408,17 @@ namespace e.Motion_Gangplank
                     var lowHealthMinion = MinionManager.GetMinions(Player.Position, Q.Range).FirstOrDefault();
                     if (lowHealthMinion != null && lowHealthMinion.Health <= Q.GetDamage(lowHealthMinion))
                         Q.Cast(lowHealthMinion);
+                }
+            }
+        }
+
+        private static void Cleanse()
+        {
+            if (W.IsReady() && Config.Item("cleanse.w").GetValue<bool>())
+            {
+                if (Buffs.Any(entry => Config.Item("cleanse.bufftypes." + entry.Key).GetValue<bool>() && Player.HasBuffOfType(entry.Value)))
+                {
+                    W.Cast();
                 }
             }
         }
