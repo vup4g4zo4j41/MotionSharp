8ac0ad02d6ebdec59ff2708154751f4988fab642
Ruben <ruben.simons@web.de>
2016-11-24 22:26:33 +0100

    Rewritten Assembly for Katarina Rework

---
+++ e.Motion Katarina/e.Motion Katarina Rework/BlockIssueOrder.cs
@@ -0,0 +1,29 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace e.Motion_Katarina
+{
+    class BlockIssueOrder
+    {
+        private static int whenToCancelR = 0;
+        public static void InitializeBlockIssueOrder()
+        {
+            Obj_AI_Base.OnIssueOrder += OnIssueOrder;
+            Obj_AI_Base.OnBuffAdd += OnBuffGain;
+        }
+
+        private static void OnBuffGain(Obj_AI_Base sender, Obj_AI_BaseBuffAddEventArgs args)
+        {
+            if(sender.IsMe && args.Buff.Name == "katarinarsound")
+            {
+                whenToCancelR = Utils.TickCount + 400;
+            }
+        }
+
+        private static void OnIssueOrder(Obj_AI_Base sender, GameObjectIssueOrderEventArgs args)
+        {
+            if (sender.IsMe && Logic.Player.HasBuff("katarinarsound") && Utils.TickCount <= whenToCancelR && Config.GetBoolValue("misc.noRCancel"))
+                args.Process = false;
+        }
+    }
+}
---
+++ e.Motion Katarina/e.Motion Katarina Rework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace e.Motion_Katarina
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if(HeroManager.Player.ChampionName != "Katarina")
+            {
+                return;
+            }
+            CustomEvents.Game.OnGameLoad += Initialize;
+        }
+        static void Initialize(EventArgs args)
+        {
+            //Various Entry Points
+            Config.InitializeMenu();
+            Logic.startLogic();
+            DaggerManager.startTracking();
+            BlockIssueOrder.InitializeBlockIssueOrder();
+        }
+    }
+}
